794dbf6f241c3c91bd6c24daca48f08938a5de0d
Drama-sp <dramakingsp@gmail.com>
2026-03-11 15:09:50 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,24 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+// Archivo esencial para que Chrome permita la instalación (PWA)
+const CACHE_NAME = 'edk-ultra-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
