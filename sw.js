9bde11dc2a793b7cc39505d71ccfd750285933cc
Drama-sp <dramakingsp@gmail.com>
2026-03-12 08:51:20 +0000

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,38 +1,19 @@
-const CACHE_NAME = 'edk-ultra-v1.3'; // Actualizamos versión para forzar recarga
+const CACHE_NAME = 'edk-ultra-v1.4';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './app_screenshot.png' // Guardamos la nueva PNG en caché
+  './edkultra.svg'
 ];
 
-// Instalar el Service Worker y guardar archivos en caché
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Estrategia: Cargar desde caché, si no hay, ir a la red
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
-});
-
-// Limpiar cachés antiguos al activar nueva versión
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-                if (key !== CACHE_NAME) {
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
 });
