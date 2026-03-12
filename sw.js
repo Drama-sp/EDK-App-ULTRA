df4f46aa27e86c2d557b5d801e9aa0f67760a145
Drama-sp <dramakingsp@gmail.com>
2026-03-12 09:05:49 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'edk-ultra-v1.4';
+// Cambiamos el nombre de la caché (v1 -> v2) para forzar la actualización
+const CACHE_NAME = 'edk-ultra-v2'; 
 const ASSETS = [
   './',
   './index.html',
@@ -6,14 +7,33 @@ const ASSETS = [
   './edkultra.svg'
 ];
 
+// Instalación: Guardar nuevos archivos
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); // Fuerza al nuevo SW a tomar el control de inmediato
+});
+
+// Activación: Borrar cachés antiguas (donde está la imagen vieja)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
